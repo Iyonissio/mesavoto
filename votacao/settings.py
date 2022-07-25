e90d89b12ca04856937f9281e3f8df45fbb27c78
Iyonissio <mbryoyo01@gmail.com>
2022-07-25 21:28:44 +0200

    update

--- votacao/settings.py
+++ votacao/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 import django_heroku
 import dj_database_url
 import decouple
@@ -110,6 +111,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
